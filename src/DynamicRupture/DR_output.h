82258a4f51a89b19316674820392d5b321a10d2d
ravil <ravil.dorozhinskii@tum.de>
2020-09-03 20:30:32 +0200

    Added PickpointParamsT and ElementwiseFaultParamsT
    * Added convertStringToMask template funtion to
      read input string masks

--- src/DynamicRupture/DR_output.h
+++ src/DynamicRupture/DR_output.h
@@ -10,6 +10,11 @@ namespace seissol {
             class FL_2;
             class FL_3;
             class FL_33;
+
+            enum class OutputType : int {None = 0,
+                                         AtPickpoint = 3,
+                                         Elementwise = 4,
+                                         AtPickpointAndElementwise = 5};
         }
     }
 }
@@ -18,11 +23,16 @@ class seissol::dr::output::Base{
 public:
     virtual ~Base() {}
 
-    void setInputParam(const YAML::Node& Params) {
+    void setInputParam(const YAML::Node& InputParams) {
       using namespace initializers;
 
-      const YAML::Node& DrParams = Params["dynamicrupture"];
-      m_Params.OutputPointType = getParamIfExists(DrParams, "outputpointtype", 3);
+      if (!InputParams["dynamicrupture"]) {
+        throw std::runtime_error("dynamic rupture params. is not provided");
+      }
+
+      const YAML::Node& DrParams = InputParams["dynamicrupture"];
+
+      m_Params.OutputPointType = static_cast<OutputType>(getParamIfExists(DrParams, "outputpointtype", 3));
       m_Params.SlipRateOutputType = getParamIfExists(DrParams, "sliprateoutputtype", 1);
       m_Params.FrictionLawType = getParamIfExists(DrParams, "fl", 0);
       m_Params.BackgroundType = getParamIfExists(DrParams, "backgroundtype", 0);
@@ -33,6 +43,74 @@ public:
       m_Params.IsGpWiseOutput = getParamIfExists(DrParams, "gpwise", false);
       m_Params.IsTermalPressureOn = getParamIfExists(DrParams, "thermalpress", false);
       m_Params.BackgroundType = getParamIfExists(DrParams, "energy_rate_printtimeinterval", 1);
+
+      m_Params.IsRfTimeOn = m_Params.IsRfOutputOn;
+
+      if (m_Params.IsDsOutputOn && !m_Params.IsRfOutputOn) {
+        m_Params.IsRfOutputOn = true;
+        m_Params.IsRfTimeOn = true;
+      }
+
+      switch (m_Params.OutputPointType) {
+        case OutputType::None:
+          break;
+
+        case OutputType::AtPickpoint:
+          // readpar_faultAtPickpoint(EQN,BND,IC,DISC,IO,CalledFromStructCode)
+          initPickpointParams(InputParams);
+          break;
+
+        case OutputType::Elementwise:
+          // readpar_faultElementwise(EQN,BND,IC,DISC,IO,CalledFromStructCode)
+          initElementWiseParams(InputParams);
+          break;
+
+        case OutputType::AtPickpointAndElementwise:
+          // readpar_faultElementwise(EQN,BND,IC,DISC,IO,CalledFromStructCode)
+          // readpar_faultAtPickpoint(EQN,BND,IC,DISC,IO,CalledFromStructCode)
+          initPickpointParams(InputParams);
+          initElementWiseParams(InputParams);
+          break;
+
+        default:
+          throw std::runtime_error("Unkown fault output type (e.g.3,4,5)");
+      }
+    }
+
+    void initPickpointParams(const YAML::Node& InputParams) {
+      using namespace initializers;
+
+      if (!InputParams["pickpoint"]) {
+        throw std::runtime_error("pickpoint output parameters for dynamic rupture is not provided");
+      }
+
+      const YAML::Node& Params = InputParams["pickpoint"];
+      m_PickpointParams.PrintTimeInterval = getParamIfExists(Params, "printtimeinterval", 1);
+      m_PickpointParams.NumOutputPoints = getParamIfExists(Params, "noutpoints", 0);
+      m_PickpointParams.PPFileName = getParamIfExists(Params, "ppfilename", std::string());
+
+      if (Params["outputmask"]) {
+        convertStringToMask(Params["outputmask"].as<std::string>(), m_PickpointParams.OutputMask);
+      }
+    }
+
+    void initElementWiseParams(const YAML::Node& InputParams) {
+      using namespace initializers;
+      if (!InputParams["elementwise"]) {
+        throw std::runtime_error("elementwise fault output parameters for dynamic rupture is not provided");
+      }
+
+      const YAML::Node& Params = InputParams["elementwise"];
+
+      m_ElementwiseParams.PrintTimeInterval = getParamIfExists(Params, "printtimeinterval", 2);
+      m_ElementwiseParams.PrintTimeIntervalSec = getParamIfExists(Params, "printtimeinterval_sec", 1.0);
+      m_ElementwiseParams.PrintIntervalCriterion = getParamIfExists(Params, "printintervalcriterion", 1);
+      m_ElementwiseParams.RefinementStrategy = getParamIfExists(Params, "refinement_strategy", 2);
+      m_ElementwiseParams.Refinement = getParamIfExists(Params, "refinement", 2);
+
+      if (Params["outputmask"]) {
+        convertStringToMask(Params["outputmask"].as<std::string>(), m_ElementwiseParams.OutputMask);
+      }
     }
 
     virtual void tiePointers(seissol::initializers::Layer&  layerData,
@@ -67,7 +145,7 @@ public:
 
 protected:
   struct ParamsT {
-    int OutputPointType{3};
+    OutputType OutputPointType{OutputType::AtPickpoint};
     int SlipRateOutputType{1};
     int FrictionLawType{0};
     int BackgroundType{0};
@@ -78,7 +156,25 @@ protected:
     bool IsGpWiseOutput{false};
     bool IsTermalPressureOn{false};
     int EnergyRatePrintTimeInterval{1};
+    bool IsRfTimeOn{false};
   } m_Params;
+
+  struct PickpointParamsT {
+    std::array<bool, 12> OutputMask{true, true, true}; // the rest is false by default
+    int PrintTimeInterval{1};
+    int NumOutputPoints{0};
+    std::string PPFileName{};
+
+  } m_PickpointParams;
+
+  struct ElementwiseFaultParamsT {
+    int PrintTimeInterval{2};
+    real PrintTimeIntervalSec{1.0};
+    int PrintIntervalCriterion{1};
+    std::array<bool, 12> OutputMask{true, true, true, true};
+    int RefinementStrategy{2};
+    int Refinement{2};
+  } m_ElementwiseParams;
 };
 
 class seissol::dr::output::FL_2 : public seissol::dr::output::Base {
--- src/Initializer/InputAux.hpp
+++ src/Initializer/InputAux.hpp
@@ -7,6 +7,10 @@
 
 namespace seissol {
   namespace initializers {
+
+    /**
+     * Returns a value from Yaml::Node converting it to from a string to the type derived from DefaultValue
+     * */
     template <typename T>
     T getParamIfExists(const YAML::Node& Param, std::string&& Field, T DefaultValue) {
       if (std::is_same<T, bool>::value) {
@@ -20,6 +24,47 @@ namespace seissol {
         return Param[Field] ? Param[Field].as<T>() : DefaultValue;
       }
     }
+
+    /**
+     * \brief Returns true if number elements in the input string (separated by the white space)
+     *  is less or equal to the size of a container
+     * */
+    template<typename OutputType, typename ContainerT>
+    bool isCapacityEnough(const std::string& InputString, ContainerT& OutputMask) {
+      std::istringstream InputStream(InputString);
+      auto Begin = std::istream_iterator<OutputType>(InputStream);
+      auto End = std::istream_iterator<OutputType>();
+
+      const int NumInputElements = std::distance(Begin, End);
+      return NumInputElements <= OutputMask.size();
+    }
+
+    /**
+     * \brief Initializes the given input mask with values from the input string
+     *
+     * \throws runtime_error if an input string contains more parameters than the capacity of a provided container
+     * */
+    template<typename ContainerT>
+    void convertStringToMask(const std::string& StringMask, ContainerT& Mask) {
+      using T = typename std::iterator_traits<typename ContainerT::iterator>::value_type;
+
+      if (!isCapacityEnough<T>(StringMask, Mask))
+        throw std::runtime_error("Num. input elements is more than the Mask capacity");
+
+      std::istringstream InputStream(StringMask);
+      auto Begin = std::istream_iterator<T>(InputStream);
+      auto End = std::istream_iterator<T>();
+
+      for (int Index = 0; Begin != End; ++Index, ++Begin) {
+        if (std::is_same<T, bool>::value) {
+          Mask[Index] = (*Begin) > 0;
+        }
+        else {
+          Mask[Index] = (*Begin);
+        }
+      }
+    }
+
   }
 }
 
