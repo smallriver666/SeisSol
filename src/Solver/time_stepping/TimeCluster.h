305881e98b5e63dfb37ab3106deba9cef96e6f41
Lukas Krenz <lukas@krenz.land>
2022-04-29 12:49:47 +0200

    Work on task scheduling

--- src/Solver/time_stepping/TimeCluster.h
+++ src/Solver/time_stepping/TimeCluster.h
@@ -384,6 +384,9 @@ private:
 
   void printTimeoutMessage(std::chrono::seconds timeSinceLastUpdate) override;
 
+  bool isRunning = false;
+  std::mutex isRunningMutex;
+
 public:
   ActResult act() override;
 
